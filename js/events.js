458a6da78d55f324b5f42f41ad5b289db21c1e10
yasser mimouni <yasmimouni2@gmail.com>
2017-12-03 15:43:14 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
 
+function getIt()
+{
+  $("p").on("click", function(e) {
+    alert("hi");
+  })
+}
+
+function frameIt()
+{
+  window.$("img").on("load", function(){
+    $("img")[0].className = "tasty";
+  })
+}
+function submitIt()
+{
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+    return;
+});
+}
+function pressIt()
+{
+  $(document).on("keydown", function(ev) {
+    ev.preventDefault();
+    if(ev.which == 71) alert("G was pressed");
+    return;
+  })
+}
+
+
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  submitIt();
 });
